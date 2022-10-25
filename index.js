2e3a9e1c6c54e8d1ec60400a62d9314ec9222bf9
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-10-25 14:59:06 +0600

    home route api is completed

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ const cors = require("cors");
 app.use(cors());
 const port = process.env.port || 5000;
 const data = require("./Databse/data.json");
+const courseDetails = require("./Databse/courseDetails.json");
 
+app.listen(port, () => {
+  console.log("server is running");
+});
+
+app.get('/home',(req,res)=>{
+    const homeData=data.data.find(h=>h.id===1);
+    res.send(homeData);
+})
