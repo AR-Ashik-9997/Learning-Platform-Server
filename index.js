783ec5e5e29c8d7e428e85e57e41b78352032890
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-10-25 15:07:09 +0600

    course-id dynamic route api is completed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 app.use(cors());
 const port = process.env.port || 5000;
 const data = require("./Databse/data.json");
-const courseDetails = require("./Databse/courseDetails.json");
+const courseDetailsData = require("./Databse/courseDetails.json");
 
 app.listen(port, () => {
   console.log("server is running");
@@ -18,4 +18,10 @@ app.get('/home',(req,res)=>{
 app.get('/course',(req,res)=>{
     const course=data.data.find(c=>c.id===2);
     res.send(course);
+})
+
+app.get('/course/:id',(req,res)=>{
+    const courseID=req.params.id;
+    const courseDetails=courseDetailsData.data.find(cd=>cd.id==courseID);    
+    res.send(courseDetails);
 })
